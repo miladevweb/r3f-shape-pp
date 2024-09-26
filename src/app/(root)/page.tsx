9bb21b722af2a28e10967033c84b897f2f9edf84
miladevweb <miladevweb@outlook.com>
2024-09-26 11:14:38 -0500

    first commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export const metadata = {
+  title: 'GSAP Transitions | Just Mila',
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,14 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
       },
     },
   },
