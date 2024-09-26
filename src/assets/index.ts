e2aa425760f9dce0f918712d911c8b547c2e7620
miladevweb <miladevweb@outlook.com>
2024-09-26 17:05:50 -0500

    add local images

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import image1 from '@/assets/image1.webp'
+import image2 from '@/assets/image2.webp'
+import image3 from '@/assets/image3.webp'
+import image4 from '@/assets/image4.webp'
+import image5 from '@/assets/image5.webp'
+import image6 from '@/assets/image6.webp'
+import image7 from '@/assets/image7.webp'
+
+export { image1, image2, image3, image4, image5, image6, image7 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,25 @@
-export const IMAGE_LIST = Array.from({ length: 7 }, (_, i) => ({
+import { image1, image2, image3, image4, image5, image6, image7 } from '@/assets'
+
+/* DEVELOPMENT IMAGES */
+const images = [image1, image2, image3, image4, image5, image6, image7]
+
+export const IMAGE_LIST = images.map((img, i) => ({
   label: `Image ${i + 1}`,
-  url: `https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage${i + 1}.webp?alt=media`,
+  url: img,
 }))
 
 export const GET_IMAGE = (i: number) => ({
   label: `Image ${i}`,
-  url: `https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage${i}.webp?alt=media`,
+  url: images[i - 1],
 })
+
+/* PRODUCTION IMAGES */
+// export const IMAGE_LIST = Array.from({ length: 7 }, (_, i) => ({
+//   label: `Image ${i + 1}`,
+//   url: `https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage${i + 1}.webp?alt=media`,
+// }))
+
+// export const GET_IMAGE = (i: number) => ({
+//   label: `Image ${i}`,
+//   url: `https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage${i}.webp?alt=media`,
+// })
